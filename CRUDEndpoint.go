f9dbf99f2b2ca3e538cb87fc2dc5f25e1ef62ed5
Tino Rusch <tino.rusch@gmail.com>
2017-09-27 13:14:43 +0200

    initial commit;

---
+++ CRUDEndpoint.go
@@ -0,0 +1,205 @@
+package crud
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+
+	"github.com/gorilla/mux"
+	uuid "github.com/satori/go.uuid"
+	log "github.com/sirupsen/logrus"
+	"github.com/trusch/streamstore"
+)
+
+// Endpoint is an http.Handler which serves CRUD requests
+type Endpoint struct {
+	router *mux.Router
+	store  streamstore.Storage
+	prefix string
+}
+
+// ServeHTTP is the function needed to implement http.Handler
+func (endpoint *Endpoint) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	endpoint.router.ServeHTTP(w, r)
+	return
+}
+
+// NewEndpoint constructs a new handler instances
+func NewEndpoint(prefix string, store streamstore.Storage) http.Handler {
+	endpoint := &Endpoint{mux.NewRouter(), store, prefix}
+	endpoint.router.Path(fmt.Sprintf("/%v", prefix)).Methods("POST").HandlerFunc(endpoint.handlePost)
+	endpoint.router.Path(fmt.Sprintf("/%v", prefix)).Methods("GET").HandlerFunc(endpoint.handleList)
+	endpoint.router.Path(fmt.Sprintf("/%v/{id}", prefix)).Methods("GET").HandlerFunc(endpoint.handleGet)
+	endpoint.router.Path(fmt.Sprintf("/%v/{id}", prefix)).Methods("PUT").HandlerFunc(endpoint.handlePut)
+	endpoint.router.Path(fmt.Sprintf("/%v/{id}", prefix)).Methods("PATCH").HandlerFunc(endpoint.handlePatch)
+	endpoint.router.Path(fmt.Sprintf("/%v/{id}", prefix)).Methods("DELETE").HandlerFunc(endpoint.handleDel)
+	return endpoint
+}
+
+func (endpoint *Endpoint) handlePost(w http.ResponseWriter, r *http.Request) {
+	log.Debugf("POST request to %v", r.URL)
+	if r.Body == nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("no body supplied"))
+		return
+	}
+	id := uuid.NewV4()
+	objectID := fmt.Sprintf("%v::%v", endpoint.prefix, id.String())
+	writer, err := endpoint.store.GetWriter(objectID)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	_, err = io.Copy(writer, r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	err = writer.Close()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte(id.String()))
+}
+
+func (endpoint *Endpoint) handleGet(w http.ResponseWriter, r *http.Request) {
+	log.Debugf("GET request to %v", r.URL)
+	vars := mux.Vars(r)
+	id := vars["id"]
+	objectID := fmt.Sprintf("%v::%v", endpoint.prefix, id)
+	if !endpoint.store.Has(objectID) {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("object not found"))
+		return
+	}
+	reader, err := endpoint.store.GetReader(objectID)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	io.Copy(w, reader)
+	reader.Close()
+}
+
+func (endpoint *Endpoint) handleList(w http.ResponseWriter, r *http.Request) {
+	log.Debugf("GET request to %v", r.URL)
+	keys, err := endpoint.store.List(endpoint.prefix)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	for id := range keys {
+		keys[id] = strings.Split(keys[id], "::")[1]
+	}
+	w.Header().Set("Content-Type", "application/json")
+	encoder := json.NewEncoder(w)
+	encoder.Encode(keys)
+}
+
+func (endpoint *Endpoint) handlePut(w http.ResponseWriter, r *http.Request) {
+	log.Debugf("PUT request to %v", r.URL)
+	if r.Body == nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("no body supplied"))
+		return
+	}
+	vars := mux.Vars(r)
+	id := vars["id"]
+	objectID := fmt.Sprintf("%v::%v", endpoint.prefix, id)
+	writer, err := endpoint.store.GetWriter(objectID)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	_, err = io.Copy(writer, r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	err = writer.Close()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	w.Write([]byte(id))
+}
+func (endpoint *Endpoint) handleDel(w http.ResponseWriter, r *http.Request) {
+	log.Debugf("DELETE request to %v", r.URL)
+	vars := mux.Vars(r)
+	id := vars["id"]
+	objectID := fmt.Sprintf("%v::%v", endpoint.prefix, id)
+	if !endpoint.store.Has(objectID) {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("object not found"))
+		return
+	}
+	err := endpoint.store.Delete(objectID)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+}
+
+func (endpoint *Endpoint) handlePatch(w http.ResponseWriter, r *http.Request) {
+	log.Debugf("PATCH request to %v", r.URL)
+	vars := mux.Vars(r)
+	id := vars["id"]
+	objectID := fmt.Sprintf("%v::%v", endpoint.prefix, id)
+	if !endpoint.store.Has(objectID) {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("object not found"))
+		return
+	}
+
+	// get old object
+	reader, err := endpoint.store.GetReader(objectID)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	oldObject := make(map[string]interface{})
+	decoder := json.NewDecoder(reader)
+	if err = decoder.Decode(&oldObject); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	// get patch object
+	decoder = json.NewDecoder(r.Body)
+	patchObject := make(map[string]interface{})
+	if err = decoder.Decode(&patchObject); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	// merge objects
+	for key, val := range patchObject {
+		oldObject[key] = val
+	}
+
+	// save object
+	writer, err := endpoint.store.GetWriter(objectID)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	encoder := json.NewEncoder(io.MultiWriter(writer, w))
+	encoder.Encode(oldObject)
+}
---
+++ CRUDEndpoint_test.go
@@ -0,0 +1,101 @@
+package crud_test
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"os"
+
+	. "github.com/trusch/crud"
+	"github.com/trusch/streamstore"
+	"github.com/trusch/streamstore/uriparser"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("CRUDEndpoint", func() {
+	var (
+		recorder *httptest.ResponseRecorder
+		handler  http.Handler
+		store    streamstore.Storage
+		err      error
+	)
+
+	BeforeEach(func() {
+		recorder = httptest.NewRecorder()
+		store, err = uriparser.NewFromURI("file:///tmp/test", nil)
+		Expect(err).NotTo(HaveOccurred())
+		handler = NewEndpoint("test", store)
+	})
+
+	AfterEach(func() {
+		os.RemoveAll("/tmp/test")
+	})
+
+	It("should be possible to post and retrieve data", func() {
+		content := "foobar"
+		code, responseData := post(handler, "/test", content)
+		Expect(code).To(Equal(http.StatusCreated))
+		Expect(responseData).NotTo(BeEmpty())
+		code, responseData = get(handler, "/test/"+responseData)
+		Expect(code).To(Equal(http.StatusOK))
+		Expect(responseData).To(Equal(content))
+	})
+
+	It("should be possible to put and retrieve data", func() {
+		content := "foobar"
+		code, responseData := put(handler, "/test/key", content)
+		Expect(code).To(Equal(http.StatusOK))
+		Expect(responseData).To(Equal("key"))
+		code, responseData = get(handler, "/test/key")
+		Expect(code).To(Equal(http.StatusOK))
+		Expect(responseData).To(Equal(content))
+	})
+
+	It("should be possible to list data", func() {
+		content := "foobar"
+		put(handler, "/test/key1", content)
+		put(handler, "/test/key2", content)
+		put(handler, "/test/key3", content)
+		put(handler, "/test/key4", content)
+		code, responseData := get(handler, "/test")
+		Expect(code).To(Equal(http.StatusOK))
+		list := []string{}
+		err := json.Unmarshal([]byte(responseData), &list)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(len(list)).To(Equal(4))
+		Expect(list).To(ConsistOf("key1", "key2", "key3", "key4"))
+	})
+
+	It("should be possible to delete data", func() {
+		content := "foobar"
+		put(handler, "/test/key1", content)
+		code, _ := del(handler, "/test/key1")
+		Expect(code).To(Equal(http.StatusOK))
+		code, _ = get(handler, "/test/key1")
+		Expect(code).To(Equal(http.StatusNotFound))
+	})
+
+	It("should be possible to patch json data fields", func() {
+		content := `{"a":1,"b":2}`
+		put(handler, "/test/key1", content)
+		patchContent := `{"c":3,"a":4}`
+		code, _ := patch(handler, "/test/key1", patchContent)
+		Expect(code).To(Equal(http.StatusOK))
+		code, resp := get(handler, "/test/key1")
+		Expect(code).To(Equal(http.StatusOK))
+		obj := make(map[string]int)
+		Expect(json.Unmarshal([]byte(resp), &obj)).To(Succeed())
+		Expect(len(obj)).To(Equal(3))
+		Expect(obj).To(HaveKeyWithValue("a", 4))
+		Expect(obj).To(HaveKeyWithValue("b", 2))
+		Expect(obj).To(HaveKeyWithValue("c", 3))
+	})
+
+	It("should return 404 for something wrong", func() {
+		req, _ := http.NewRequest("GET", "/wrong", nil)
+		handler.ServeHTTP(recorder, req)
+		Expect(recorder.Code).To(Equal(http.StatusNotFound))
+	})
+})
